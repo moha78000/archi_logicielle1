3719e4e17e131d373d5c3d16f056b0ec19d8797a
22302683 <moh78078000@gmail.com>
2025-03-17 11:34:40 +0100

    ajout du .env

--- src/archilog/views/__init__.py
+++ src/archilog/views/__init__.py
@@ -2,17 +2,19 @@ from dataclasses import dataclass
 from flask import Flask
 from archilog.views.web_ui import web_ui
 from archilog.views.cli import cli
+from dotenv import load_dotenv
+import os
 
 @dataclass
 class Config:
     DATABASE_URL: str
     DEBUG: bool
-    SECRET_KEY: str
 
 config = Config(
-    DATABASE_URL="sqlite:///data.db",
-    DEBUG=True,
-    SECRET_KEY="secret01"
+    DATABASE_URL=os.getenv("ARCHILOG_DATABASE_URL", ""),
+    DEBUG=os.getenv("ARCHILOG_DEBUG", "False") == "True"
+
+    
 
 )
 
@@ -20,10 +22,13 @@ config = Config(
 
 def create_app():
     app = Flask(__name__)
-
+    
+    load_dotenv()
 
     # Enregistrement des blueprints
     app.register_blueprint(web_ui)
-    app.config.from_object(config)
+     # Configuration de l'application
+    app.config["SECRET_KEY"] = config.getenv("SECRET_KEY", "fallback_secret")
     app.cli.add_command(cli)  # Ici on attache Click à l'application Flask
+    
     return app
